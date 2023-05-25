d24c78b18fd2e81300393078cda24a880a97ffce
Juancaceres963 <dnijuancaceres@gmail.com>
2023-05-25 20:13:01 -0300

    handleClear para limpiar el form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,45 +1,81 @@
 import React, { useState } from "react";
-import {Sub} from '../components/types';
+import { Sub } from "../components/types";
 
 interface FormState {
-    inputValue: Sub
+  inputValue: Sub;
 }
 
 interface FormProps {
-    onNewSub: (newSub: Sub) => void
+  onNewSub: (newSub: Sub) => void;
 }
 
-const Form = ({onNewSub}: FormProps) => {
-const [inputValue, setInputValue] = useState<FormState["inputValue"]>({
-    nick: '',
+const Form = ({ onNewSub }: FormProps) => {
+  const [inputValue, setInputValue] = useState<FormState["inputValue"]>({
+    nick: "",
     subMonths: 0,
-    avatar: '',
-    description: ''
-})
+    avatar: "",
+    description: "",
+  });
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        onNewSub(inputValue)
-    }
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onNewSub(inputValue);
+    handleClear();
+  };
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        setInputValue({
-            ...inputValue,
-            [e.target.name]: e.target.value
-        })
-    }
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setInputValue({
+      ...inputValue,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input onChange={handleChange} value={inputValue.nick} type='text' name="nick" placeholder="nick" />
-                <input onChange={handleChange} value={inputValue.subMonths}type='number' name="subMonths" placeholder="subMonths" />
-                <input onChange={handleChange} value={inputValue.avatar}type='text' name="avatar" placeholder="avatar" />
-                <textarea onChange={handleChange} value={inputValue.description} name="description" placeholder="description" />
-                <button>Save and sub!</button>
-            </form>
-        </div>
-    )
-}
+  const handleClear = () => {
+    setInputValue({
+      nick: "",
+      subMonths: 0,
+      avatar: "",
+      description: "",
+    });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          value={inputValue.nick}
+          type="text"
+          name="nick"
+          placeholder="nick"
+        />
+        <input
+          onChange={handleChange}
+          value={inputValue.subMonths}
+          type="number"
+          name="subMonths"
+          placeholder="subMonths"
+        />
+        <input
+          onChange={handleChange}
+          value={inputValue.avatar}
+          type="text"
+          name="avatar"
+          placeholder="avatar"
+        />
+        <textarea
+          onChange={handleChange}
+          value={inputValue.description}
+          name="description"
+          placeholder="description"
+        />
+        <button onClick={handleClear} type="button">Clear the form</button>
+        <button type="submit">Save and sub!</button>
+      </form>
+    </div>
+  );
+};
 
 export default Form;
